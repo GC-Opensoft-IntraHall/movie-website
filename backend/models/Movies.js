a14e2994241d322f390367968206f1034a1efd58
Rahul Prasad <rahulrnc03@gmail.com>
2025-01-18 13:33:19 +0530

    First Commit

---
+++ backend/models/Movies.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+// Referencing an existing collection
+
+// Perform CRUD operations on the existing 'notes' collection
+export const Movies = mongoose.model('movies', new mongoose.Schema({}, { strict: false }));
---
+++ backend/routes/movies.js
@@ -0,0 +1,42 @@
+import express from 'express';
+const router = express.Router();
+import { Movies } from '../models/Movies.js';
+
+//ROUTE 1:Get All the Notes using GET:"/api/movies/fetchAllMovies".login Required.
+
+router.get('/fetchAllMovies',  async (req, res) => {
+    try {
+        const movies = await Movies.find().limit(10);   // find is used because we need to find 'all' notes of a user.
+        res.json(movies); // req.user can be used because we have imported fetchuser.
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+})
+
+//ROUTE 2: Search movies by keywords
+
+router.get('/search',  async (req, res) => {
+    try {
+        
+        const result= await Movies.aggregate([
+            {
+                $search: {
+                  index: "search-movies",
+                  text: {
+                    query: req.query.t,
+                    path: {
+                      wildcard: "*"
+                    }
+                  }
+                }
+              }
+        ])
+        res.json(result);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+})
+
+export default router;
